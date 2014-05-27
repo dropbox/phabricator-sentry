56bd0a887e07edb33abee467f82ce284d5ed9a04
David Cramer <cramer@dropbox.com>
2014-05-27 15:07:16 -0700

    Simple collection at shutdown
    We unfortunately cannot override Phabricator's error listener as it gets called after our hooks.
    
    Instead we instrument simple message capture (not ideal).

--- src/sentrylogger/SentryLogger.php
+++ src/sentrylogger/SentryLogger.php
@@ -9,33 +9,17 @@
 final class SentryLogger {
 
   private static $client = null;
-  private static $errorHandler = null;
 
-  public static function setClient($client, $error_handler=null) {
+  public static function setClient($client) {
     self::$client = $client;
-    if (!$error_handler) {
-      $error_handler = new Raven_ErrorHandler($client);
-    }
-    self::$errorHandler = $error_handler;
   }
 
-  public static function handleErrors($event, $value, $metadata) {
-    if (!self::$client) {
-      return;
-    }
-    switch ($event) {
-      case PhutilErrorHandler::EXCEPTION:
-        // $value is of type Exception
-        self::$client->captureException($value);
-        break;
-      case PhutilErrorHandler::ERROR:
-        // $value is a simple string
-        self::$errorHandler->handleError($metadata['error_code'],
-          $value,
-          $metadata['file'],
-          $metadata['line'],
-          $metadata['context']);
-        break;
+  public static function logAllPendingErrors() {
+    $errors = DarkConsoleErrorLogPluginAPI::getErrors();
+
+    // TODO(dcramer): add support for the 'trace' attribute in the client API
+    foreach ($errors as $error) {
+      self::$client->captureMessage($error['details']);
     }
   }
 
--- src/sentrylogger/SentryLoggerRegistration.php
+++ src/sentrylogger/SentryLoggerRegistration.php
@@ -3,14 +3,25 @@
 class SentryLoggerRegistration extends PhabricatorAutoEventListener {
   public function register() {
     $sentry_dsn = PhabricatorEnv::getEnvConfigIfExists('sentry.dsn');
+
     if (empty($sentry_dsn)) {
       return;
     }
 
+    Raven_Autoloader::register();
+
+    // Configure the client
     $client = new Raven_Client($sentry_dsn);
 
+    // Install error handlers
+    $error_handler = new Raven_ErrorHandler($client);
+    $error_handler->registerExceptionHandler();
+    $error_handler->registerErrorHandler();
+
+    // We can't override DarkConsole, so let's capture them at the
+    // end of the request
     SentryLogger::setClient($client);
-    PhutilErrorHandler::setErrorListener(array('SentryLogger', 'handleErrors'));
+    register_shutdown_function(array('SentryLogger', 'logAllPendingErrors'));
   }
 
   // must be stubbed due to parent class

5648b8fc838ed28a41f3e50b3966d19b950778c8
David Cramer <cramer@dropbox.com>
2014-05-27 12:53:17 -0700

    Instantiate client with DSN

--- src/sentrylogger/SentryLoggerRegistration.php
+++ src/sentrylogger/SentryLoggerRegistration.php
@@ -1,8 +1,15 @@
 <?php
 
 class SentryLoggerRegistration extends PhabricatorAutoEventListener {
-	public function register() {
-		SentryLogger::setClient($client);
- 		PhutilErrorHandler::setErrorListener(array('SentryLogger', 'handleErrors'));
- 	}
+  public function register() {
+    $sentry_dsn = PhabricatorEnv::getEnvConfigIfExists('sentry.dsn');
+    if (empty($sentry_dsn)) {
+      return;
+    }
+
+    $client = new Raven_Client($sentry_dsn);
+
+    SentryLogger::setClient($client);
+    PhutilErrorHandler::setErrorListener(array('SentryLogger', 'handleErrors'));
+  }
 }
